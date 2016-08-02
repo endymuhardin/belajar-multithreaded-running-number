f322b1711212e2bf44c1b48e183280d62cdd5e09
Endy Muhardin <endy.muhardin@gmail.com>
2016-08-02 14:53:21 +0700

    implementasi lock mode serializable / pessimistic locking

--- src/main/java/com/muhardin/endy/belajar/database/isolation/dao/RunningNumberDao.java
+++ src/main/java/com/muhardin/endy/belajar/database/isolation/dao/RunningNumberDao.java
@@ -2,8 +2,11 @@ package com.muhardin.endy.belajar.database.isolation.dao;
 
 import com.muhardin.endy.belajar.database.isolation.entity.RunningNumber;
 import java.util.Date;
+import javax.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface RunningNumberDao extends PagingAndSortingRepository<RunningNumber, Long>{
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     public RunningNumber findByCurrentDate(Date tanggalKerja);
 }
